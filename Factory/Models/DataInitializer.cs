5c3954f0cf750b27dd9a69a4dfb3faf054191409
Austin Papritz <austin@papritz.dev>
2023-08-04 13:17:10 -0700

    created DataInitializer, hope it works

--- Factory/Models/DataInitializer.cs
+++ Factory/Models/DataInitializer.cs
@@ -4,26 +4,62 @@ using System;
 using System.Linq;
 using System.Collections.Generic;
 
-
-namespace Factory.Models;
-
-public class DataInitializer
+namespace Factory.Models
 {
-    public static void InitializeData(WebApplication app)
+    public class DataInitializer
     {
-        using (var scope = app.Services.CreateScope())
+        public static void InitializeData(WebApplication app)
         {
-            var context = scope.ServiceProvider.GetRequiredService<FactoryContext>();
-            context.Database.Migrate();
+            using (var scope = app.Services.CreateScope())
+            {
+                var context = scope.ServiceProvider.GetRequiredService<FactoryContext>();
+                context.Database.Migrate();
 
-            // If there's already stuff in db, don't run.
-            if (context.License.Any())
-            {
-                return;
-            }
-            else
-            {
-                return;
+                // Don't exe if db is already seeded.
+                if (!context.Engineers.Any())
+                {
+                    // List of Licenses.
+                    var licenses = new List<License>
+                    {
+                        new License { Name = "Heavy" },
+                        new License { Name = "HighTemp" },
+                        new License { Name = "Hydraulics" },
+                        new License { Name = "Emissions" }
+                    };
+                    context.Licenses.AddRange(licenses);
+                    context.SaveChanges();
+
+                    // Seeding 5 Engineers with random licenses.
+                    var rand = new Random();
+                    for (int i = 1; i <= 5; i++)
+                    {
+                        var engineer = new Engineer
+                        {
+                            Name = $"Engineer{i}",
+                            EngineerLicenses = licenses
+                                .Where(l => rand.Next(2) == 0) // For every license, pick num less than 2 (0 or 1), if the random number is 0 include that license.
+                                .Select(l => new EngineerLicense { License = l }) // Create an `EngineerLicense` for every License selected above.
+                                .ToList()
+                        };
+                        context.Engineers.Add(engineer);
+                    }
+
+                    // Seeding 15 Machines with random licenses.
+                    for (int i = 1; i <= 15; i++)
+                    {
+                        var machine = new Machine
+                        {
+                            Make = $"Make{i}",
+                            Model = $"Model{i}",
+                            MachineLicenses = licenses
+                                .Where(l => rand.Next(2) == 0) // For every license, pick num less than 2 (0 or 1), if the random number is 0 include that license.
+                                .Select(l => new MachineLicense { License = l }) // Create a `MachineLicense` for every License selected above.
+                                .ToList()
+                        };
+                        context.Machines.Add(machine);
+                    }
+                    context.SaveChanges();
+                }
             }
         }
     }
