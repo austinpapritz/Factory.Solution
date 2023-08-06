839dd5810d8283e8e2f7079ff67620df677ed664
Austin Papritz <austin@papritz.dev>
2023-08-05 21:12:33 -0700

    you can create a new license

--- Factory/Controllers/LicensesController.cs
+++ Factory/Controllers/LicensesController.cs
@@ -22,4 +22,24 @@ public class LicensesController : Controller
         return View("Form");
     }
 
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Create([Bind("Name")] License license)
+    {
+        if (ModelState.IsValid)
+        {
+            // Add license to Licenses table.
+            _db.Licenses.Add(license);
+            _db.SaveChanges();
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        // If anything goes wrong, reload form.
+
+        ViewData["FormAction"] = "Create";
+        ViewData["SubmitButton"] = "Add License";
+        return View("Form");
+    }
+
 }
